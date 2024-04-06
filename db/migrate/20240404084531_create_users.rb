9e9c32ea1f2492f4ed4f4b08e232c1f335a46e4e
Kian-F <52603628+Kian-F@users.noreply.github.com>
2024-04-06 14:47:27 +1100

    db migration

--- db/migrate/20240404084531_create_users.rb
+++ db/migrate/20240404084531_create_users.rb
@@ -2,13 +2,11 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :first_name
-      t.string :las_name
+      t.string :last_name
       t.string :phone
       t.string :email, null: false, index: { unique: true }
       t.string :password_digest, null: false
-
       t.boolean :verified, null: false, default: false
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_04_084532) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_06_034453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_04_084532) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "las_name"
+    t.string "last_name"
     t.string "phone"
     t.string "email", null: false
     t.string "password_digest", null: false
