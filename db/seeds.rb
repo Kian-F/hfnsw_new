d978134d8f307ea5dfe0f1bd3d08fa1316bb42fa
Kian-F <52603628+Kian-F@users.noreply.github.com>
2024-04-17 21:46:26 +1000

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Organization.create(name: "HFNSW")
+Organization.create(title: "HFNSW")
 
 roles = [ 
   { title: "Admin", key: "admin" },
